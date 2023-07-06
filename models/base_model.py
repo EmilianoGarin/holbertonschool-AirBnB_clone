9e3f632c59382bc78f1cb5fa7a8a0781ed71c9d3
EmilianoGarin <60578409+EmilianoGarin@users.noreply.github.com>
2023-07-06 14:54:48 -0700

    Base_model, task 3 and 4

---
+++ models/base_model.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+"""
+    Class base
+"""
+from datetime import datetime
+import uuid
+
+
+class BaseModel():
+    """
+        Class base
+    """
+    def __init__(self, *args, **kwargs):
+        """ initialize"""
+        if len(kwargs) != 0:
+            for k, v in kwargs.items():
+                if k != '__class__':
+                    if k == 'created_at' or k == 'updated_at':
+                        time = datetime.strptime(v,"%Y-%m-%dT%H:%M:%S.%f")
+                        setattr(self, k, time)
+                    else:
+                        setattr(self, k, v)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+
+    def __str__(self) -> str:
+        """string with a description of a simple object type"""
+        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
+    
+    def save(self):
+        """saves the moment it was modified"""
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        """description of a simple object type"""
+        des = self.__dict__
+        des['created_at'] = des['created_at'].isoformat()
+        des['updated_at'] = des['updated_at'].isoformat()
+        des['__class__'] = self.__class__
+        return des
