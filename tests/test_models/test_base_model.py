b9d3699120b42ad6508774a59a54f5a1ee39df33
EmilianoGarin <60578409+EmilianoGarin@users.noreply.github.com>
2023-07-07 17:12:57 -0700

    add test in test_base_model and test_file_storage

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -3,18 +3,79 @@
     Unittest of BaseModel
 """
 import unittest
+import os
 from models.base_model import BaseModel
 import datetime
 
 
 class TestBaseModel(unittest.TestCase):
     """
-        Test base
+        Test the class BaseModel
     """
 
     def setUp(self) -> None:
         """
             sets up objects for testing later
         """
-        self.test_model1 = BaseModel()
-        self.test_model2 = BaseModel()
+        self.model1 = BaseModel()
+        self.model2 = BaseModel()
+
+    def test_attributes(self):
+        """
+            test type and existence of all atributes
+        """
+        self.assertTrue(self.model1, "id")
+        self.assertIs(type(self.model1.id), str)
+        self.assertTrue(self.model1, "created_at")
+        self.assertIs(type(self.model1.created_at), datetime.datetime)
+        self.assertTrue(self.model1, "updated_at")
+        self.assertIs(type(self.model1.updated_at), datetime.datetime)
+
+    def test_singularity(self):
+        """
+            test thet ids are unique
+        """
+        self.assertNotEqual(self.model1.id, self.model2.id)
+
+    def test_doc(self):
+        """
+            test documentation
+        """
+        self.assertIsNotNone(BaseModel.__doc__)
+
+    def test_str_(self):
+        """
+            test __str__ method
+        """
+        model = self.model1
+        self.assertEqual(
+            f"[{type(model).__name__}] ({model.id}) {model.__dict__}",
+            str(model))
+
+    def test_save(self):
+        """
+            test save method
+        """
+        update_at = self.model1.__dict__['updated_at']
+        self.model1.save()
+        self.assertTrue(os.path.isfile("file.json"))
+        self.assertNotEqual(self.model1.__dict__['updated_at'], update_at)
+
+    def test_to_dict(self):
+        """
+            test to_dict method
+        """
+        dict = self.model1.to_dict()
+        self.assertNotEqual(dict, self.model1.__dict__)
+        self.assertEqual(dict['id'], self.model1.__dict__['id'])
+        self.assertNotEqual(dict['created_at'],
+                            self.model1.__dict__['created_at'])
+        self.assertNotEqual(type(dict['created_at']),
+                            type(self.model1.__dict__['created_at']))
+        self.assertNotEqual(dict['updated_at'],
+                            self.model1.__dict__['updated_at'])
+        self.assertNotEqual(type(dict['updated_at']),
+                            type(self.model1.__dict__['updated_at']))
+
+    if __name__ == '__main__':
+        unittest.main()
---
+++ tests/test_models/test_engine/test_file_storage.py
@@ -0,0 +1,86 @@
+#!/usr/bin/python3
+"""
+    Unittest of FileStorage
+"""
+import unittest
+import os
+import models
+from models.engine.file_storage import FileStorage
+from models.base_model import BaseModel
+
+
+
+class TestFileStorage(unittest.TestCase):
+    """
+        Test the class FileStorage
+    """
+    def setUp(self):
+        self.storage = FileStorage()
+        self.model1 = BaseModel()
+        self.model2 = BaseModel()
+
+    def tearDown(self):
+        try:
+            os.remove('file.json')
+        except FileNotFoundError:
+            pass
+
+    def test_models_storage(self):
+        """
+            test if storage in init is on
+        """
+        self.assertIsNotNone(models.storage.all())
+        self.assertIsNotNone(models.storage.all())
+
+    def test_attributes(self):
+        """
+            test type and existence of all atributes
+        """
+        self.assertEqual(dict, type(self.storage.all()))
+
+    def test_doc(self):
+        """
+            test documentation
+        """
+        self.assertIsNotNone(FileStorage.__doc__)
+
+    def test_all(self):
+        """
+            test all method
+        """
+        self.storage.new(self.model1)
+        self.storage.new(self.model2)
+        all_objs = self.storage.all()
+        self.assertIn("BaseModel." + self.model1.id, all_objs)
+        self.assertIn("BaseModel." + self.model2.id, all_objs)
+        self.assertIs(all_objs, self.storage._FileStorage__objects)
+        self.assertEqual(all_objs, self.storage._FileStorage__objects)
+
+    def test_new(self):
+        """
+            test new method
+        """
+        self.storage.new(self.model1)
+        obj_key = "BaseModel." + self.model1.id
+        self.assertIn(obj_key, self.storage.all())
+        self.assertEqual(self.model1, self.storage.all()[obj_key])
+
+    def test_save_reload(self):
+        """
+            test save and reload method
+        """
+        self.storage.new(self.model1)
+        self.storage.save()
+        self.storage = FileStorage()
+        self.storage.reload()
+        obj_key = "BaseModel." + self.model1.id
+        obj_all = self.storage.all()
+        obj_storage = obj_all[obj_key]
+        self.assertIn(obj_key, self.storage.all())
+        self.assertTrue(self.model1.__dict__ == obj_storage.__dict__)
+        self.assertFalse(self.model1 is obj_storage)
+        self.assertIsInstance(obj_storage, BaseModel)
+        self.assertEqual(self.model1.id, obj_storage.id)
+
+    if __name__ == '__main__':
+        unittest.main()
